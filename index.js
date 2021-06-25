13f0bca9ed9019a667e40d1da66918f086bb930e
kyle.hipolito <kyle.hipolito@quasset.com>
2021-06-25 14:08:09 +0800

    Fixed

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const koa = require("koa");
 const views = require("koa-views");
 const serve = require("koa-static");
 const router = require("@koa/router")();
+const koa = require("koa");
 const http2 = require("http2");
 const https = require("https");
 const fs = require("fs");
 
-const key = fs.readFileSync("./certs/key.pem", "UTF8");
-const cert = fs.readFileSync("./certs/cert.pem", "UTF8");
 
 const app = new koa();
 
@@ -20,27 +18,15 @@ const render = views(__dirname + "/views", {
 app.use(render);
 
 router.get("/", async function (ctx) {
-  const grid = [];
-
-  for (let i = 0; i < 9; i++) {
-    const row = [];
-    for (let j = 1; j <= 13; j++) {
-      const root = "/images";
-      const path = `${root}/${i * 13 + j}.jpg`;
-      row.push(path);
-    }
-
-    grid.push(row);
-  }
-
-  await ctx.render("index", {
-    images: grid,
-  });
+  await ctx.render("index");
 });
 
 app.use(router.routes());
 
 app.use(serve("./static"));
 
-https.createServer({ key, cert }, app.callback()).listen(3000);
-http2.createSecureServer({ key, cert }, app.callback()).listen(3001);
+const key = fs.readFileSync("./certs/key.pem", "UTF8");
+const cert = fs.readFileSync("./certs/cert.pem", "UTF8");
+
+https.createServer({ key, cert }, app.callback()).listen(3000, () => console.log('https on port 3000'));
+http2.createSecureServer({ key, cert }, app.callback()).listen(3001, () => console.log('http2 on port 3001'));
